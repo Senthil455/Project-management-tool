6ca84d17cd4364752a65b13b35fcee5a3e04b2dd
Senthil Raja R <senthilrajasen637@gmail.com>
2025-03-25 18:32:55 +0530

    feat: add asyncHandler wrapper

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -1,3 +1,9 @@
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
+export const isValidId = (id) => mongoose.Types.ObjectId.isValid(id);
+
+export const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
+export const generateToken = (id) =>
