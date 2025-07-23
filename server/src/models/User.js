da544fe6903ca35b634e4688367cb843342ec2e6
Senthil Raja R <senthilrajasen637@gmail.com>
2025-07-23 10:58:10 +0530

    fix: fix password hash comparison

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -15,11 +15,3 @@ const userSchema = new mongoose.Schema(
     avatarColor: { type: String, default: '#0052CC' },
   },
   { timestamps: true }
-);
-
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
