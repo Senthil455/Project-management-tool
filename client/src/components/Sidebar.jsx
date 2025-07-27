39d68cbd5df4d0f37e8c82ebc7a5bb3f94663bb7
Senthil Raja R <senthilrajasen637@gmail.com>
2025-07-27 23:59:09 +0530

    feat: scaffold Sidebar layout

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -111,9 +111,3 @@ export default function Sidebar({ onProjectCreated }) {
 
 export const hashColor = (str) => {
   const colors = ['#0052CC', '#00B8D9', '#36B37E', '#FF8B00', '#FF5630', '#6554C0', '#2684FF', '#00875A', '#BF2600', '#5243AA'];
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  return colors[Math.abs(hash) % colors.length];
-};
