bdef6e2d9824ae44939245d56a25f200b26a7680
Senthil Raja R <senthilrajasen637@gmail.com>
2025-07-27 23:34:25 +0530

    feat: create Avatar component

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -8,20 +8,3 @@ export default function Avatar({ user, size = 32, showName = false, className = 
   }
   const initials = user.name
     .split(' ')
-    .map((w) => w[0])
-    .filter(Boolean)
-    .slice(0, 2)
-    .join('')
-    .toUpperCase();
-  return (
-    <span className={`avatar-wrap ${className}`} title={user.name}>
-      <span
-        className="avatar"
-        style={{ width: size, height: size, fontSize: size * 0.38, background: user.avatarColor || '#0052CC' }}
-      >
-        {initials}
-      </span>
-      {showName && <span className="avatar-name">{user.name}</span>}
-    </span>
-  );
-}
