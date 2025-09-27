1cf46f321d23c7b54e93569301f75fc3e33f835d
Senthil Raja R <senthilrajasen637@gmail.com>
2025-09-27 11:16:38 +0530

    fix: fix CORS origin for production

--- server/src/index.js
+++ server/src/index.js
@@ -11,3 +11,8 @@ import { notFound, errorHandler } from './middleware/error.js';
 dotenv.config();
 connectDB();
 
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '1mb' }));
+
