f7d928872435cec655a3689b970e813cd365347b
Senthil Raja R <senthilrajasen637@gmail.com>
2026-02-26 13:04:48 +0530

    feat: add JWT token helper

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -1,10 +1,4 @@
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-
-export const isValidId = (id) => mongoose.Types.ObjectId.isValid(id);
-
-export const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
+import jwt from "jsonwebtoken";
 
 export const generateToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
