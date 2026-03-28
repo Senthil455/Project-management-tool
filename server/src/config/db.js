808043860ca98e44bace00f230abb49be4def17e
Senthil Raja R <senthilrajasen637@gmail.com>
2026-03-28 10:26:11 +0530

    refactor: finalize database connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error(err.message);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
