5b63d572ac080cb5c0d881a3813616f85bbbf15b
Senthil Raja R <senthilrajasen637@gmail.com>
2026-04-28 21:25:29 +0530

    refactor: finalize helper utilities

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -1,4 +1,10 @@
-import jwt from "jsonwebtoken";
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+export const isValidId = (id) => mongoose.Types.ObjectId.isValid(id);
+
+export const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 export const generateToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
