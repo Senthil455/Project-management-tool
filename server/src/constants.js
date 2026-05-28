3b9908363771c201a8278b8bcae74097cf866ac5
Senthil Raja R <senthilrajasen637@gmail.com>
2026-05-28 17:59:26 +0530

    feat(server): add shared domain constants

---
+++ server/src/constants.js
@@ -0,0 +1,30 @@
+const ISSUE_TYPES = {
+  TASK: "task",
+  BUG: "bug",
+  STORY: "story",
+};
+
+const ISSUE_STATUSES = {
+  BACKLOG: "backlog",
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+};
+
+const ISSUE_PRIORITIES = {
+  LOW: "low",
+  MEDIUM: "medium",
+  HIGH: "high",
+};
+
+const ROLES = {
+  OWNER: "owner",
+  MEMBER: "member",
+};
+
+module.exports = {
+  ISSUE_TYPES,
+  ISSUE_STATUSES,
+  ISSUE_PRIORITIES,
+  ROLES,
+};
